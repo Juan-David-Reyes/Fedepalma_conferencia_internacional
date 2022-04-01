4abcb45bcf89b2e10a63b72081c3fc0fca26ed83
Juan David <juan.reyes@paxzu.co>
2022-04-01 11:31:39 -0500

    avance header y footer con responsive

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.bars_menu').click(function(){
+        $('.navbar_desk ul').toggleClass('active_nav_m');
+    });
+    $('.close_mobile').click(function(){
+        $('.navbar_desk ul').toggleClass('active_nav_m');
+    });
+});
