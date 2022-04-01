6c4ab35ab8b20edf86c68b660ce5cf5178737997
Juan David <juan.reyes@paxzu.co>
2022-04-01 12:58:27 -0500

    slider hero OK

--- js/main.js
+++ js/main.js
@@ -6,3 +6,19 @@ $(document).ready(function(){
         $('.navbar_desk ul').toggleClass('active_nav_m');
     });
 });
+
+function sliderHero(){
+    const swiper = new Swiper(".cont_gen_hero .sliderHero", {
+        navigation: {
+          nextEl: ".cont_gen_hero .swiper-button-next",
+          prevEl: ".cont_gen_hero .swiper-button-prev",
+        },
+        effect: 'fade',
+        fadeEffect: {
+            crossFade: true
+        },
+        parallax: true,
+      });
+}
+
+sliderHero();
